740f0da12ec23f1d3904ccc5b8ddbc33707b0912
millerkalvin <Millerkalvin@gmail.com>
2019-08-24 00:50:16 -0400

    Update smorse.js

--- Challenges/smorse.js
+++ Challenges/smorse.js
@@ -1,5 +1,7 @@
 // This challenge comes from this reddit post:
 // https://www.reddit.com/r/dailyprogrammer/comments/cmd1hb/20190805_challenge_380_easy_smooshed_morse_code_1/
+
+
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
 const morseAlphabet = ".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..";
 
