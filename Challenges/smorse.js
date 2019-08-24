ae27ea845b59b2ff722b94cfeb242e55e3db96b7
millerkalvin <Millerkalvin@gmail.com>
2019-08-24 00:45:22 -0400

    Update smorse.js

--- Challenges/smorse.js
+++ Challenges/smorse.js
@@ -1,1 +1,26 @@
-console.log('hello world');
+// This challenge comes from this reddit post:
+// https://www.reddit.com/r/dailyprogrammer/comments/cmd1hb/20190805_challenge_380_easy_smooshed_morse_code_1/
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const morseAlphabet = ".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..";
+
+const morseAlphabetArray = morseAlphabet.split(" ");
+const alphabetArray = alphabet.split("");
+
+function smorse(word){
+	
+	let morseWord = "";
+	
+	for(let i = 0; i < word.length; i++){
+		
+		let letter = word.charAt(i);
+		
+		let letterIndex = alphabetArray.findIndex((arrayLetter) => arrayLetter == letter);
+		
+		morseWord = morseWord + morseAlphabetArray[letterIndex];
+		
+	}
+	
+	return morseWord;
+}
+
+console.log("The word 'sos' is " + smorse("sos"));
